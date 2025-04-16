947b3f26666a10a5f01f887f8ab425aff2906270
Adrien Berchet <adrien.berchet@gmail.com>
2025-04-16 11:02:23 +0200

    WIP: Almost working except cache

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,6 +17,8 @@ from geoalchemy2.alembic_helpers import _monkey_patch_get_indexes_for_sqlite
 from . import copy_and_connect_sqlite_db
 from . import get_postgis_major_version
 from . import get_postgres_major_version
+from . import get_versions
+from . import print_versions
 from .schema_fixtures import *  # noqa
 
 
@@ -236,6 +238,9 @@ def engine(tmpdir, db_url, _engine_echo, _require_all_dialects):
             else:
                 pytest.skip(reason=msg)
 
+    with current_engine.begin() as connection:
+        versions = get_versions(connection)
+    print_versions(versions)
     yield current_engine
     current_engine.dispose()
 
